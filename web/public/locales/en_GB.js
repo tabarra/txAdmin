bbd0412a994db185b93114632dbd84d5cc689683
rex2630 <rex2630@gmail.com>
2019-08-20 23:10:11 +0200

    Updated czech

--- web/public/locales/en_GB.js
+++ web/public/locales/en_GB.js
@@ -1,11 +1,26 @@
 // English translation
 R.registerLocale('en-GB', 
 {
+// Menu
+  'dash': " Palubka",
+  'lv': " Konzole",
+  'sets': " Nastavení",
+  'amngr': " Správce adminů",
+  'script': " Skripty",
+  'acts': " Záznam aktivit",
+  'report': " Celkové info o stavu",
+  'exte': "DOPLŇKY",
+  'add3': " Přidat doplněk",
+  'host2': "Využití serveru",
+  'cpu': "VYUŽITÍ CPU",
+  'ram': "VYUŽITÍ RAM",
+  'gen6': "Obecné",
+  'new': "NOVÉ",
+  'new2': "NOVÉ",
 // Dashboard
   'cmd': "Příkaz",
   'cmds': "Příkazy:",
   'title': "{{serverName}} Palubka",
-  'dash': " Palubka",
   'msg': "Odeslat admin zprávu",
   'sel': "&gt; Zvolit",
   'restart': "Zapnout/Restartovat skript",
@@ -25,5 +40,51 @@ R.registerLocale('en-GB',
   'ctrl-start': "ZAPNOUT",
   'ctrl-restart': "RESTARTOVAT",
   'ctrl-stop': "ZASTAVIT",
-  'log': "Záznam adminování"
+// Action Log
+  'log': "Záznam adminování",
+// Admin manager
+  'manager': "Správce adminů",
+  'user': "Přezdívka",
+  'per': "Oprávnění",
+  'action': "Akce",
+  'all': "Všichni admini",
+  'modAddAdmin-save': "Uložit",
+  'cancel': "Zrušit",
+  'addmin': "Přidat admina",
+// Settings page
+  'settings': "Nastavení",
+  'nav-global-tab': "Obecné",
+  'nav-monitor-tab': "Sledování/Restartování",
+// FXServer console
+  'console2': "FXServer konzole",
+  'clearConsole': "Pročistit konzoli",
+  'download': "Stáhnout záznam",
+  'toggleAutoScroll': "Vypnout skrolování",
+// Player list
+  'list': "Seznam hráčů:",
+  'load': "Načítání",
+  'close': "Zavřít",
+  'close2': "Zavřít",
+// Resources list
+  'res': "Seznam skriptů",
+  'allres': "Všechny skripty",
+  'statis': "Stav",
+  'nm': "Název",
+// Footer
+  'discord': "Promluv si o tom na Discordu",
+// Full Status Report
+  'os': "Typ OS:",
+  'os2': "Vydání OS:",
+  'usernum': "Uživatelské jméno:",
+  'hcpu': "CPU na serveru:",
+  'hmem': "RAM na serveru",
+  'ver': "Verze:",
+  'status1': "Stav: ",
+  'cfg': "txAdmin konfigurace:",
+  'maxc': "Max. hráčů",
+  'tag': "Značky:",
+  'scrap': "Skriptů:",
+// Extension Tab
+  'addee': "Přidat doplněk",
+  'soon': "Již brzy...",
 });
