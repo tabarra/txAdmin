967270622dd763038e4a156789d9973e13657dbc
rex2630 <rex2630@gmail.com>
2019-08-21 11:19:37 +0200

    Delete

--- web/public/locales/de_DE.js
+++
@@ -1,8 +0,0 @@
-// German translation
-R.registerLocale('de-DE', 
-{
-  'subtitle': "Das ist ein Beispiel zur Demonstration lokaler HTML Internationalisierung mit <a href='https://github.com/keithamus/R.js' target='_blank'>R.js</a>",
-  'test1': "Ich bin ein Text mit einer Nummer %i",
-  'test2': "Ich bin ein weiterer Text mit einem Text %s",
-  'dash': "über"
-});
