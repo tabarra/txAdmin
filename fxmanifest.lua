778bc41aa7a66ff9b37acdbfcb4c6cd957c8614e
Andre Tabarra <tabarra@vexguard.net>
2020-10-10 23:21:37 -0300

    updated packages + version bump

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ games { 'gta5' }
 author 'Tabarra'
 description 'Remotely Manage & Monitor your GTA5 FiveM Server'
 repository 'https://github.com/tabarra/txAdmin'
-version '2.7.1'
+version '2.7.2'
 
 
 -- NOTE: Due to global package constraints, js scripts will be loaded from main.js
