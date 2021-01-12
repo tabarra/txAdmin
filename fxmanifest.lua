f4bb580bbe0d5640187bfaebfe64b2d2e713c26c
Andre Tabarra <tabarra@vexguard.net>
2021-01-12 12:39:54 -0300

    version bump

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@
 author 'Tabarra'
 description 'Remotely Manage & Monitor your GTA5 FiveM Server'
 repository 'https://github.com/tabarra/txAdmin'
-version '3.2.1'
+version '3.2.2'
 
 fx_version 'bodacious'
 games { 'gta5' }
