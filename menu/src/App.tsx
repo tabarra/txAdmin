7c1fbcfa3b79e8aa6bb2b2044b543989800ae02a
Christopher <onlygamernerd@gmail.com>
2021-04-27 02:40:00 +0200

    initial menu commit

---
+++ menu/src/App.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import './App.css';
+
+export default function App() {
+  const [count, setCount] = useState<number>(0);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button className="App" onClick={() => setCount(count + 1)}>Increase</button>
+    </div>
+  )
+}
+
+// hello
+
