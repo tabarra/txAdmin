de753e474041b4ba9a882f2c81e0baea64f02feb
Taso <taso@projecterror.dev>
2021-04-28 22:09:24 -0400

    feat(menu/hooks): useArrowKeys custom hook
    A simple custom hook to be used for keyboard navigation

---
+++ menu/src/hooks/useArrowKeys.tsx
@@ -0,0 +1,51 @@
+import { useEffect } from 'react'
+
+interface KeyCallbacks {
+  onLeftDown?: () => void
+  onRightDown?: () => void
+  onUpDown?: () => void
+  onDownDown?: () => void
+}
+
+/**
+ * A simple hook for listening to arrow key down events
+ *
+ * Note: Might change this up a little bit so its more of a global event listener
+ *
+ * @param onLeftDown - Left arrow handler function
+ * @param onRightDown - Right arrow handler function
+ * @param onUpDown - Up arrow handler function
+ * @param onDownDown - Down arrow handler function
+ */
+
+export const useArrowKeys = ({onLeftDown, onRightDown, onUpDown, onDownDown}: KeyCallbacks) => {
+  useEffect(() => {
+    // Our basic handler function for keydown events
+    const keyHandler = (e: KeyboardEvent) => {
+      switch (e.code) {
+       case "ArrowLeft":
+         e.preventDefault()
+         onLeftDown()
+         break;
+       case "ArrowRight":
+         e.preventDefault()
+         onRightDown()
+         break;
+       case "ArrowUp":
+         e.preventDefault()
+         onUpDown()
+         break;
+       case "ArrowDown":
+         e.preventDefault()
+         onDownDown()
+         break;
+     }
+    }
+
+    // Add that boi
+    window.addEventListener('keydown', keyHandler)
+
+    // Remove on cleanup
+    return () => window.removeEventListener('keydown', keyHandler)
+  }, [onLeftDown, onRightDown, onUpDown, onDownDown])
+}
