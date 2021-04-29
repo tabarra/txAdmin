665edc070fb76a9b397e73f5502a9b63340086d1
Taso <taso@projecterror.dev>
2021-04-28 22:08:44 -0400

    tweak(menu/theme): Tweaked global theming

--- menu/src/styles/theme.tsx
+++ menu/src/styles/theme.tsx
@@ -2,6 +2,16 @@ import { createMuiTheme } from '@material-ui/core'
 
 export const MenuTheme = createMuiTheme({
   palette: {
-    type: "dark"
+    type: "dark",
+    primary: {
+      main: "rgba(0,197,140,0.87)",
+    },
+    secondary: {
+      main: "#fff"
+    },
+    text: {
+      secondary: 'rgba(221,221,221,0.54)',
+      primary: '#fff'
+    }
   }
 })
