f4d754caccd9f012a5d2721ad22e3bb9a179cc58
クリストファー <chip@projecterror.dev>
2021-04-29 23:23:20 +0200

    Fix/tab selector (#334)

--- menu/src/components/PageTabs.tsx
+++ menu/src/components/PageTabs.tsx
@@ -1,8 +1,15 @@
 import React, { ChangeEvent } from "react";
-import { Box, Tab, Tabs } from "@material-ui/core";
+import { Box, makeStyles, Tab, Tabs, Theme } from "@material-ui/core";
 import { usePageContext } from "../provider/PageProvider";
 
+const useStyles = makeStyles((theme: Theme) => ({
+  tab: {
+    minWidth: '100px',
+  }
+}))
+
 export const PageTabs: React.FC = () => {
+  const classes = useStyles();
   const { page, setPage } = usePageContext();
 
   const handleChange = (event: ChangeEvent<{}>, newValue: number) => {
@@ -10,16 +17,17 @@ export const PageTabs: React.FC = () => {
   };
 
   return (
-    <Box width="100%">
+      <Box width="100%">
       <Tabs
         value={page}
+        centered
         indicatorColor="primary"
         textColor="secondary"
         onChange={handleChange}
       >
-        <Tab label="Main" wrapped />
-        <Tab label="Players" wrapped />
-        <Tab label="TXAdmin" wrapped />
+        <Tab className={classes.tab} label="Main" wrapped />
+        <Tab className={classes.tab}label="Players" wrapped />
+        <Tab className={classes.tab} label="TXAdmin" wrapped />
       </Tabs>
     </Box>
   );
