85092f830f4b88dd54fee04ca33102f2688385c2
Taso <taso@projecterror.dev>
2021-04-28 22:11:29 -0400

    feat(menu/main): Basic main tab layout

---
+++ menu/src/components/MenuListItem.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {ListItem, ListItemIcon, ListItemText, makeStyles, Theme} from "@material-ui/core";
+
+interface MenuListItemProps {
+  icon: JSX.Element
+  primary: string,
+  secondary: string
+  onSelect: () => void
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    borderRadius: 15
+  },
+  icon: {
+    color: theme.palette.text.secondary
+  },
+  overrideText: {
+    color: theme.palette.text.primary,
+    fontSize: 16,
+  }
+}))
+
+export const MenuListItem: React.FC<MenuListItemProps> = ({icon, primary, onSelect, secondary}) => {
+  const classes = useStyles()
+
+  return (
+    <ListItem onClick={() => onSelect()} button className={classes.root} dense>
+      <ListItemIcon className={classes.icon}>{icon}</ListItemIcon>
+      <ListItemText
+        primary={primary}
+        secondary={secondary}
+        classes={{
+          primary: classes.overrideText
+        }}
+      />
+    </ListItem>
+  )
+}
