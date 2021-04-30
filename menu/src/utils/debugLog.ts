6b57fb8e733393f8721b5bbe1568a59bba5223cd
Taso <taso@projecterror.dev>
2021-04-29 21:14:55 -0400

    docs(menu): Misc inline tsdocs for util functions

--- menu/src/utils/debugLog.ts
+++ menu/src/utils/debugLog.ts
@@ -1,5 +1,13 @@
 import config from '../utils/config.json'
 
+/**
+ * Simple debug logger for development use.
+ * Used in `fetchNui` and `useNuiEvent`.
+ *
+ * @param action - The action of this debug log
+ * @param data - Data you wish to debug
+ * @param context - Optional context
+ */
 export const debugLog = (action: string, data: unknown, context: string = 'Unknown') => {
   if (config.DEBUG_MODE) {
     console.group(`${context} | Action: ${action}`)
