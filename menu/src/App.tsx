70882809bf1f9963b96245dc03ce123577783879
Taso <taso@projecterror.dev>
2021-04-29 21:04:59 -0400

    refactor(menu/useNuiEvent): Refactored to fit menu use case
    Also mounted handlers for changing global state from a window MessageEvent

--- menu/src/App.tsx
+++ menu/src/App.tsx
@@ -3,24 +3,28 @@ import "./App.css";
 import { useIsMenuVisible } from "./atoms/visibility.atom";
 import MenuRoot from "./components/MenuRoot";
 import { SnackbarProvider } from "./provider/SnackbarProvider";
-import {DialogProvider} from "./provider/DialogProvider";
-import {useEscapeListener} from "./hooks/useEscapeListener";
+import { DialogProvider } from "./provider/DialogProvider";
+import { useEscapeListener } from "./hooks/useEscapeListener";
+import { useNuiListenerService } from "./hooks/useNuiListenersService";
 
 const App: React.FC = () => {
   const visible = useIsMenuVisible();
-  useEscapeListener()
+
+  // These hooks are pretty should always be mounted
+  useEscapeListener();
+  useNuiListenerService()
 
   return (
     <SnackbarProvider>
-        <div className="App">
-          <DialogProvider>
-            {/*
+      <div className="App">
+        <DialogProvider>
+          {/*
               Fade API seems to not like this here
               will probably need to do a manual transition
             */}
-            {visible && <MenuRoot />}
-          </DialogProvider>
-        </div>
+          {visible && <MenuRoot />}
+        </DialogProvider>
+      </div>
     </SnackbarProvider>
   );
 };
