f0148762ca7775385c0fd35dfc9f0d3b42774b25
Taso <taso@projecterror.dev>
2021-05-01 16:53:04 -0400

    tweak(menu/players): Debounce user input

---
+++ menu/src/hooks/useDebouce.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+export const useDebounce = (value: unknown, delay: number = 100) => {
+    const [debouncedValue, setDebouncedValue] = useState(value);
+
+    useEffect(
+      () => {
+        const handler = setTimeout(() => {
+          setDebouncedValue(value);
+        }, delay);
+        return () => {
+          clearTimeout(handler);
+        };
+      },
+      [value, delay]
+    );
+
+    return debouncedValue;
+}
