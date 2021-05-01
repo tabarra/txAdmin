51fb160b84647a18f3752249cd1faa37ee92a2a7
GoatGeek <goatgeek@relta.net>
2021-05-01 10:24:30 -0700

    fix(menu/scripts): Exclude menu server script from being executed in the monitor process feat(menu/scripts): Revive shared.lua (works fine?) fix(menu/scripts): Re-added access checking events so the menu loads

--- scripts/menu/server/sv_menu.lua
+++ scripts/menu/server/sv_menu.lua
@@ -1,9 +1,54 @@
-local oneSyncEnabled = false
+if GetConvar('txAdminServerMode', 'false') ~= 'true' then
+  return
+end
+
+local ServerCtxObj = {
+  oneSync = {
+    type = nil,
+    status = false
+  },
+  projectName = nil,
+  maxClients = 30
+}
 
 -- OneSync check
-if GetConvar('onesync', 'off') == 'on' or 'legacy' then
-  oneSyncEnabled = true
-end
+-- In case of hot reloads attach this event handler at start of
+-- script init, Client Event that queries GlobalState are queued
+-- at the **end** of resource init.
+AddEventHandler('onResourceStart', function(resourceName)
+  if resourceName ~= GetCurrentResourceName() then
+    return
+  end
+
+  local oneSyncConvar = GetConvar('onesync', 'off')
+  if oneSyncConvar == ('on' or 'legacy') then
+    ServerCtxObj.oneSync.type = oneSyncConvar
+    ServerCtxObj.oneSyncEnabled = true
+  elseif oneSyncConvar == 'off' then
+    ServerCtxObj.oneSyncStatus = false
+  end
+
+  -- Default '' in fxServer
+  local svProjectName = GetConvar('sv_projectname', '')
+  if svProjectName ~= '' then
+    ServerCtxObj.projectName = svProjectName
+  end
+
+  -- Default 30 in fxServer
+  local svMaxClients = GetConvarInt('sv_maxclients', 30)
+  ServerCtxObj.maxClients = svMaxClients
+
+  debugPrint('Server CTX assigned to GlobalState, CTX:')
+  debugPrint(json.encode(ServerCtxObj))
+
+  -- In case this was a hot reload of monitor, we send init event to all online clients
+  GlobalState.txAdminServerCtx = ServerCtxObj
+end)
+
+RegisterServerEvent('txAdmin:menu:getServerCtx', function()
+  local src = source
+  TriggerClientEvent('txAdmin:menu:sendServerCtx', src, serverCtxObj)
+end)
 
 RegisterServerEvent('txAdmin:menu:checkAccess', function()
   local src = source
