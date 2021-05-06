6ba7f9191f63dc407267a6cb197fa8f10d16bac1
Taso <taso@projecterror.dev>
2021-05-05 20:12:25 -0400

    fix(menu/listener-service): Fix invalid hook call for ServerCtx

--- menu/src/hooks/useNuiListenersService.tsx
+++ menu/src/hooks/useNuiListenersService.tsx
@@ -16,42 +16,43 @@ interface SnackbarAlert {
 }
 
 interface AnnounceMessageProps {
-  message: string
+  message: string;
 }
 
-const AnnounceMessage: React.FC<AnnounceMessageProps> = ({message}) => (
+const AnnounceMessage: React.FC<AnnounceMessageProps> = ({ message }) => (
   <Box maxWidth={200}>
-    <Typography style={{fontWeight: 'bold'}}>Server Announcement</Typography>
+    <Typography style={{ fontWeight: "bold" }}>Server Announcement</Typography>
     {message}
   </Box>
-)
+);
 
 // Passive Message Event Listeners & Handlers for global state
 export const useNuiListenerService = () => {
   const setVisible = useSetIsMenuVisible();
   const setPlayerState = useSetPlayersState();
   const setMenuPage = useSetPage();
+  const setServerCtx = useSetServerCtx();
   const { enqueueSnackbar } = useSnackbar();
 
   useNuiEvent<boolean>("setVisible", setVisible);
   useNuiEvent<PlayerData[]>("setPlayerState", setPlayerState);
   useNuiEvent<txAdminMenuPage>("setMenuPage", setMenuPage);
-  useNuiEvent("setServerCtx", useSetServerCtx)
+  useNuiEvent("setServerCtx", setServerCtx);
 
   useNuiEvent<SnackbarAlert>("setSnackbarAlert", ({ level, message }) => {
     enqueueSnackbar(message, {
-      variant: level
-    })
+      variant: level,
+    });
   });
 
   useNuiEvent("addAnnounceMessage", ({ message }: { message: string }) => {
-    enqueueSnackbar(<AnnounceMessage message={message}/>, {
+    enqueueSnackbar(<AnnounceMessage message={message} />, {
       variant: "warning",
       title: "Server Announcement",
       anchorOrigin: {
         horizontal: "right",
-        vertical: "top"
+        vertical: "top",
       },
-    })
-  })
+    });
+  });
 };
