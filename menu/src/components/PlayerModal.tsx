3cdb9389513a43fa5359644f6fe21e3f1a4c116a
Christopher <onlygamernerd@gmail.com>
2021-05-11 21:04:38 +0200

    feat(menu/player-modal): fix selected list item

--- menu/src/components/PlayerModal.tsx
+++ menu/src/components/PlayerModal.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
+  Box,
   Button,
   Dialog,
   DialogActions,
@@ -11,14 +12,34 @@ import {
   ListItem,
   ListItemText,
   TextField,
+  Typography,
+  Theme,
+  makeStyles,
+  useTheme
 } from "@material-ui/core";
 import { usePlayerDetails } from "../state/players.state";
 import { usePlayerModal, useTabs } from "../provider/PlayerProvider";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  overrideDialogPaper: {
+    backgroundColor: theme.palette.background.default,
+  }
+}))
+
+const useDialogStyles = makeStyles((theme: Theme) => ({
+  root: {
+    '&$selected, &$selected:hover': {
+      background: theme.palette.primary.main,
+    }
+  },
+  selected: {}
+}))
 
 const PlayerModal: React.FC = () =>  {
   const player = usePlayerDetails();
   const { tab } = useTabs();
   const { modal, setModal } = usePlayerModal();
+  const theme = useTheme();
 
   if (!player) return null;
 
@@ -29,23 +50,33 @@ const PlayerModal: React.FC = () =>  {
     }
   }
 
-
   return (
-    <Dialog open={modal} fullWidth onClose={handleClose}>
+    <Dialog 
+      open={modal} 
+      fullWidth 
+      onClose={handleClose} 
+      maxWidth="md" 
+      PaperProps={{
+        style: {
+          backgroundColor: theme.palette.background.default,
+          minHeight: 600
+        },
+      }}
+    >
       <DialogTitle>
         [{player.id}] {player.username}
       </DialogTitle>
-        <Grid container spacing={8}>
-          <Grid item xs={3} md={3}>
+        <Box display="flex" px={2}>
+          <Box minWidth={200}>
             <DialogList />
-          </Grid>
-          <Grid item xs={8}>
+          </Box>
+          <Box flexGrow={1} mt={-2}>
             {tab == 1 && <DialogInfoView />}
             {tab == 2 && <DialogIdView />}
             {tab == 4 && <DialogBanView />} 
-          </Grid>
-        </Grid>
-        <DialogActions>
+          </Box>
+        </Box>
+        <DialogActions style={{ marginRight: 30 }}>
           <Button color="primary">
             DM
           </Button>
@@ -56,24 +87,25 @@ const PlayerModal: React.FC = () =>  {
             Warn
           </Button>
         </DialogActions>
-    </Dialog>
+      </Dialog>
   )
 }
 
 const DialogList: React.FC = () => {
-  const { setTab } = useTabs();
+  const { tab, setTab } = useTabs();
+  const classes = useDialogStyles();
   return (
-    <List disablePadding>
-      <ListItem button onClick={() => setTab(1)} divider>
+    <List>
+      <ListItem button onClick={() => setTab(1)} divider selected={tab === 1 && true} classes={{ root: classes.root, selected: classes.selected }}>
         <ListItemText primary="Info" />
       </ListItem>
-      <ListItem button onClick={() => setTab(2)} divider>
+      <ListItem button onClick={() => setTab(2)} divider selected={tab === 2 && true} classes={{ root: classes.root, selected: classes.selected }}>
         <ListItemText primary="IDs" />
       </ListItem>
-      <ListItem button divider>
+      <ListItem button onClick={() => setTab(3)} divider classes={{ root: classes.root, selected: classes.selected }}>
         <ListItemText primary="History" />
       </ListItem>
-      <ListItem button onClick={() => setTab(4)} divider>
+      <ListItem button onClick={() => setTab(4)} divider selected={tab === 4 && true} classes={{ root: classes.root, selected: classes.selected }}>
         <ListItemText primary="Ban" />
       </ListItem>
     </List>
@@ -81,11 +113,11 @@ const DialogList: React.FC = () => {
 }
 
 const DialogInfoView: React.FC = () => (
-  <DialogContent>
-    <DialogContentText>Player Info</DialogContentText>
-    <DialogContentText>Session Time: 0 minutes</DialogContentText>
-    <DialogContentText>Play time: --</DialogContentText>
-    <DialogContentText>Joined: May 4, 2021 - 14:25:15</DialogContentText>
+  <DialogContent >
+    <Typography variant="h6">Player Info</Typography>
+    <Typography>Session Time: 0 minutes</Typography>
+    <Typography>Play time: --</Typography>
+    <Typography>Joined: May 4, 2021 - 14:25:15</Typography>
     <TextField
       autoFocus
       margin="dense"
