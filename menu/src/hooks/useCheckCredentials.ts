e73484549fad0e7da74bb2b66bf0c7f0ae4074e2
Taso <taso@projecterror.dev>
2021-05-27 22:15:43 -0400

    tweak(hooks/useCheckCredentials): initiate WebPipe call on mount
    Will fetch for relevant admin data for the current user and populate recoil state

--- menu/src/hooks/useCheckCredentials.ts
+++ menu/src/hooks/useCheckCredentials.ts
@@ -4,15 +4,15 @@ import {
   useSetPermissions,
 } from "../state/permissions.state";
 import { debugLog } from "../utils/debugLog";
-import { useNuiEvent } from "./useNuiEvent";
+import { useEffect } from 'react';
 
 export const usesCheckCredentials = () => {
   const setPermsState = useSetPermissions();
 
-  useNuiEvent("getNuiCredentials", () => {
+  useEffect(() => {
     fetchWebPipe<PermCheckServerResp>("/auth/nui").then((result) => {
-      debugLog("Get Auth Data", result);
+      debugLog("Get Auth Data", result, 'WebPipeReq');
       setPermsState(result);
     });
-  });
+  }, [])
 };
