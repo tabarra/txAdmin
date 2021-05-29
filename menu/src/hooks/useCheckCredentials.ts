22221c9b8da4fed04a67cafec9de303692dd6347
Taso <taso@projecterror.dev>
2021-05-29 10:00:19 -0400

    tweak(menu/fetch-auth): set mock perms in browser env

--- menu/src/hooks/useCheckCredentials.ts
+++ menu/src/hooks/useCheckCredentials.ts
@@ -13,6 +13,18 @@ export const usesCheckCredentials = () => {
     fetchWebPipe<PermCheckServerResp>("/auth/nui").then((result) => {
       debugLog("Get Auth Data", result, 'WebPipeReq');
       setPermsState(result);
+    }).catch(e => {
+      if (!process.env.IN_GAME) {
+        debugLog('Browser AuthData', 'Detected browser mode, dispatching mock auth data', 'WebPipeReq')
+        setPermsState({
+          expiration: 100000,
+          isAdmin: true,
+          permissions: ['all_permissions']
+        })
+        return
+      }
+
+      throw e
     });
   }, [])
 };
