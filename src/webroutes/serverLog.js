03bf6ceb5918b283669510eb3194f370183432db
Andre Tabarra <tabarra@vexguard.net>
2021-05-29 20:13:18 -0300

    fix(menu): serverlog crashes on player name click

--- src/webroutes/serverLog.js
+++ src/webroutes/serverLog.js
@@ -104,7 +104,7 @@ function processPlayerData(src) {
 
     let name = xss(src.name).replace(/"/g, '&quot;');
     let identifiers = xss(src.identifiers.join(';')).replace(/"/g, '&quot;');
-    return `<a href="/serverLog#!" data-player-identifiers="${identifiers}" data-player-name="${name}" class="text-primary event-source">${name}</a>`;
+    return `<span data-player-identifiers="${identifiers}" data-player-name="${name}" class="text-primary event-source">${name}</span>`;
 }
 
 
