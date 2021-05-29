9f4b890d3c6a61964b5b039ba15e270022c4ae29
Taso <taso@projecterror.dev>
2021-05-29 17:09:43 -0400

    tweak(menu/IFrameProvider): Bust URL on menu visible change

--- menu/src/provider/IFrameProvider.tsx
+++ menu/src/provider/IFrameProvider.tsx
@@ -49,7 +49,8 @@ export const IFrameProvider: React.FC = ({ children }) => {
   // Will reset the iFrame page to server logs everytime
   useEffect(() => {
     if (isMenuVisible) {
-      setCurFramePg(PATH_ON_PAGE_LOAD);
+      const refreshBuster = Math.random().toString().padStart(8, '0').slice(-8);
+      setCurFramePg(`/${PATH_ON_PAGE_LOAD}?refresh${refreshBuster}`);
     }
   }, [isMenuVisible]);
 
