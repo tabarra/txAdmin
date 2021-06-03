e8fd34b4e132eed71868c3d0f4a7e26ff0a64144
Andre Tabarra <tabarra@vexguard.net>
2021-06-03 12:48:43 -0300

    fix(menu): reverted unwanted change

--- scripts/menu/client/cl_menu.lua
+++ scripts/menu/client/cl_menu.lua
@@ -565,99 +565,17 @@ RegisterNetEvent('txAdmin:menu:spawnVehicle', function(netID)
         seatsToPlace[i] = pedInSeat
       end
     end
-  end
-  oldVehVelocity = 0.0
-end)
-
-local isSpectateEnabled = false
-local storedTargetPed = nil
-local storedTargetPlayerId
-local storedGameTag = nil
-
-local function createGamerTagInfo()
-  if storedGameTag and IsMpGamerTagActive(storedGameTag) then return end
-  local nameTag = ('[%d] %s'):format(GetPlayerServerId(storedTargetPlayerId), GetPlayerName(storedTargetPlayerId))
-  storedGameTag = CreateFakeMpGamerTag(storedTargetPed, nameTag, false, false, '', 0, 0, 0, 0)
-  SetMpGamerTagVisibility(storedGameTag, 2, 1)  --set the visibility of component 2(healthArmour) to true
-  SetMpGamerTagAlpha(storedGameTag, 2, 255) --set the alpha of component 2(healthArmour) to 255
-  SetMpGamerTagHealthBarColor(storedGameTag, 129) --set component 2(healthArmour) color to 129(HUD_COLOUR_YOGA)
-  SetMpGamerTagVisibility(storedGameTag, 4, NetworkIsPlayerTalking(i))
-  --debugPrint(('Created gamer tag for ped (%s), TargetPlayerID (%s)'):format(storedTargetPlayerId, storedTargetPlayerId))
-end
-
-local function clearGamerTagInfo()
-  if not storedGameTag then return end
-  RemoveMpGamerTag(storedGameTag)
-  storedGameTag = nil
-end
-
---- Will freeze the player and set the entity to invisible
---- @param bool boolean - Whether we should prepare or cleanup
-local function preparePlayerForSpec(bool)
-  local playerPed = PlayerPedId()
-  FreezeEntityPosition(playerPed, bool)
-end
-
---- Will toggle spectate for a targeted ped
---- @param targetPed nil|number - The target ped when toggling on, can be nil when toggling off
-local function toggleSpectate(targetPed, targetPlayerId)
-  local playerPed = PlayerPedId()
-
-  if isSpectateEnabled then
-    isSpectateEnabled = false
-    clearPersistentAlert('spectateCtx')
-
-    if not lastSpectateLocation then
-      error('Last location previous to spectate was not stored properly')
-    end
-
-    if not storedTargetPed then
-      error('Target ped was not stored to unspectate')
-    end
-
-    RequestCollisionAtCoord(lastSpectateLocation.x, lastSpectateLocation.y, lastSpectateLocation.z)
-    SetEntityCoords(playerPed, lastSpectateLocation.x, lastSpectateLocation.y, lastSpectateLocation.z)
-    -- The player is still frozen while we wait for collisions to load
-    while not HasCollisionLoadedAroundEntity(playerPed) do
-      Wait(5)
-    end
-    debugPrint('Collisions loaded around player')
-
-    preparePlayerForSpec(false)
-    debugPrint('Unfreezing current player')
-
-    NetworkSetInSpectatorMode(false, storedTargetPed)
-    debugPrint(('Set spectate to false for targetPed (%s)'):format(storedTargetPed))
-    clearGamerTagInfo()
-
-    storedTargetPed = nil
+    debugPrint("Deleting existing vehicle (" .. oldVeh .. ")")
+    DeleteVehicle(oldVeh)
   else
-    storedTargetPed = targetPed
-    storedTargetPlayerId = targetPlayerId
-    local targetCoords = GetEntityCoords(targetPed)
-
-    sendPersistentAlert('spectateCtx', 'info', ('You are spectating: %s'):format(GetPlayerName(targetPlayerId)))
-
-    debugPrint(('Targets coords = x: %f, y: %f, z: %f'):format(targetCoords.x, targetCoords.y, targetCoords.z))
-
-    RequestCollisionAtCoord(targetCoords.x, targetCoords.y, targetCoords.z)
-    while not HasCollisionLoadedAroundEntity(targetPed) do
-      Wait(5)
-    end
-    debugPrint(('Collisions loaded around TargetPed (%s)'):format(targetPed))
-
-    NetworkSetInSpectatorMode(true, targetPed)
-    debugPrint(('Now spectating TargetPed (%s)'):format(targetPed))
-    isSpectateEnabled = true
+    seatsToPlace[-1] = ped
   end
-end
-
-
-RegisterCommand('txAdmin:menu:endSpectate', function()
-  if isSpectateEnabled then
-    toggleSpectate(storedTargetPed)
-    preparePlayerForSpec(false)
-    sendSnackbarMessage('info', 'Exiting spectate mode')
+  
+  local tries = 0
+  while not NetworkDoesEntityExistWithNetworkId(netID) do
+    tries = tries + 1
+    if tries > 250 then break end
+    Wait(10)
   end
   local veh = NetworkGetEntityFromNetworkId(netID)
   if not veh or veh == 0 then error("Vehicle did not spawn") end
@@ -672,30 +590,6 @@ RegisterCommand('txAdmin:menu:endSpectate', function()
   end
 end)
 
-RegisterNetEvent('txAdmin:menu:specPlayerResp', function(playerId, coords)
-  local playerPed = PlayerPedId()
-  local clientPlayerId = GetPlayerFromServerId(playerId)
-
-  if clientPlayerId == PlayerId() then
-    return sendSnackbarMessage('error', 'You cannot spectate yourself!')
-  end
-
-  lastSpectateLocation = GetEntityCoords(playerPed)
-
-  SetEntityCoords(playerPed, coords.x, coords.y, coords.z - 15.0, 0, 0, 0, false)
-  preparePlayerForSpec(true)
-
-  ---- We need to wait to make sure that the player is actually available once we teleport
-  ---- this can take some time so we do this
-  repeat
-    Wait(50)
-    debugPrint('Waiting for player to resolve')
-    clientPlayerId = GetPlayerFromServerId(playerId)
-  until (GetPlayerPed(clientPlayerId) > 0) and clientPlayerId ~= -1
-
-  debugPrint('Target Ped sucessfully found!')
-  toggleSpectate(GetPlayerPed(clientPlayerId), clientPlayerId)
-end)
 
 CreateThread(function()
   while true do
