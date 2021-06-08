bfedfe520e7886cb5f187a404f45af857e42891c
Taso <taso@projecterror.dev>
2021-06-07 23:57:54 -0400

    tweak(menu/player-modal): Add tooltips to troll action buttons

--- menu/src/components/PlayerModal/Tabs/DialogActionView.tsx
+++ menu/src/components/PlayerModal/Tabs/DialogActionView.tsx
@@ -4,6 +4,8 @@ import {
   Button,
   DialogContent,
   makeStyles,
+  Tooltip,
+  TooltipProps,
   Typography,
 } from "@material-ui/core";
 import {
@@ -36,6 +38,9 @@ const useStyles = makeStyles({
     rowGap: 10,
     paddingBottom: 15,
   },
+  tooltipOverride: {
+    fontSize: 12,
+  },
 });
 
 const DialogActionView: React.FC = () => {
@@ -54,22 +59,38 @@ const DialogActionView: React.FC = () => {
       return showNoPerms("Message");
 
     openDialog({
-      title: `${t("nui_menu.player_modal.actions.moderation.dm_dialog.title")} ${assocPlayer.username}`,
-      description: t("nui_menu.player_modal.actions.moderation.dm_dialog.description"),
-      placeholder: t("nui_menu.player_modal.actions.moderation.dm_dialog.placeholder"),
+      title: `${t(
+        "nui_menu.player_modal.actions.moderation.dm_dialog.title"
+      )} ${assocPlayer.username}`,
+      description: t(
+        "nui_menu.player_modal.actions.moderation.dm_dialog.description"
+      ),
+      placeholder: t(
+        "nui_menu.player_modal.actions.moderation.dm_dialog.placeholder"
+      ),
       onSubmit: (reason: string) => {
         fetchWebPipe<TxAdminAPIResp>("/player/message", {
           method: "POST",
           data: {
             id: assocPlayer.id,
-            message: reason
-          }
-        }).then(resp => {
-          enqueueSnackbar(t("nui_menu.player_modal.actions.moderation.dm_dialog.dm_sent"), { variant: translateAlertType(resp.type) })
-        }).catch(e => {
-          enqueueSnackbar(t("nui_menu.player_modal.actions.moderation.dm_dialog.unknown_error"), { variant: 'error' })
-          console.error(e)
+            message: reason,
+          },
         })
+          .then((resp) => {
+            enqueueSnackbar(
+              t("nui_menu.player_modal.actions.moderation.dm_dialog.dm_sent"),
+              { variant: translateAlertType(resp.type) }
+            );
+          })
+          .catch((e) => {
+            enqueueSnackbar(
+              t(
+                "nui_menu.player_modal.actions.moderation.dm_dialog.unknown_error"
+              ),
+              { variant: "error" }
+            );
+            console.error(e);
+          });
       },
     });
   };
@@ -78,25 +99,46 @@ const DialogActionView: React.FC = () => {
     if (!userHasPerm("players.warn", playerPerms)) return showNoPerms("Warn");
 
     openDialog({
-      title: `${t("nui_menu.player_modal.actions.moderation.warn_dialog.title")} ${assocPlayer.username}`,
-      description: t("nui_menu.player_modal.actions.moderation.warn_dialog.description"),
-      placeholder: 'Reason...',
+      title: `${t(
+        "nui_menu.player_modal.actions.moderation.warn_dialog.title"
+      )} ${assocPlayer.username}`,
+      description: t(
+        "nui_menu.player_modal.actions.moderation.warn_dialog.description"
+      ),
+      placeholder: "Reason...",
       onSubmit: (reason: string) => {
         fetchWebPipe<TxAdminAPIResp>("/player/warn", {
           method: "POST",
           data: {
             id: assocPlayer.id,
-            reason: reason
-          }
-        }).then(resp => {
-          if (resp.type === 'danger') {
-            return enqueueSnackbar(t("nui_menu.player_modal.actions.moderation.warn_dialog.unknown_error"), { variant: 'error' })
-          }
-          enqueueSnackbar(t("nui_menu.player_modal.actions.moderation.warn_dialog.warn_sent"), { variant: translateAlertType(resp.type) })
-        }).catch(e => {
-          enqueueSnackbar(t("nui_menu.player_modal.actions.moderation.warn_dialog.unknown_error"), { variant: 'error' })
-          console.error(e)
+            reason: reason,
+          },
         })
+          .then((resp) => {
+            if (resp.type === "danger") {
+              return enqueueSnackbar(
+                t(
+                  "nui_menu.player_modal.actions.moderation.warn_dialog.unknown_error"
+                ),
+                { variant: "error" }
+              );
+            }
+            enqueueSnackbar(
+              t(
+                "nui_menu.player_modal.actions.moderation.warn_dialog.warn_sent"
+              ),
+              { variant: translateAlertType(resp.type) }
+            );
+          })
+          .catch((e) => {
+            enqueueSnackbar(
+              t(
+                "nui_menu.player_modal.actions.moderation.warn_dialog.unknown_error"
+              ),
+              { variant: "error" }
+            );
+            console.error(e);
+          });
       },
     });
   };
@@ -105,25 +147,48 @@ const DialogActionView: React.FC = () => {
     if (!userHasPerm("players.kick", playerPerms)) return showNoPerms("Kick");
 
     openDialog({
-      title: `${t("nui_menu.player_modal.actions.moderation.kick_dialog.title")} ${assocPlayer.username}`,
-      description: t("nui_menu.player_modal.actions.moderation.kick_dialog.description"),
-      placeholder: t("nui_menu.player_modal.actions.moderation.kick_dialog.placeholder"),
+      title: `${t(
+        "nui_menu.player_modal.actions.moderation.kick_dialog.title"
+      )} ${assocPlayer.username}`,
+      description: t(
+        "nui_menu.player_modal.actions.moderation.kick_dialog.description"
+      ),
+      placeholder: t(
+        "nui_menu.player_modal.actions.moderation.kick_dialog.placeholder"
+      ),
       onSubmit: (reason: string) => {
         fetchWebPipe<TxAdminAPIResp>("/player/kick", {
           method: "POST",
           data: {
             id: assocPlayer.id,
-            reason: reason
-          }
-        }).then(resp => {
-          if (resp.type === 'danger') {
-            return enqueueSnackbar(t("nui_menu.player_modal.actions.moderation.kick_dialog.unknown_error"), { variant: 'error' })
-          }
-          enqueueSnackbar(t("nui_menu.player_modal.actions.moderation.kick_dialog.kick_sent"), { variant: translateAlertType(resp.type) })
-        }).catch(e => {
-          enqueueSnackbar(t("nui_menu.player_modal.actions.moderation.kick_dialog.unknown_error"), { variant: 'error' })
-          console.error(e)
+            reason: reason,
+          },
         })
+          .then((resp) => {
+            if (resp.type === "danger") {
+              return enqueueSnackbar(
+                t(
+                  "nui_menu.player_modal.actions.moderation.kick_dialog.unknown_error"
+                ),
+                { variant: "error" }
+              );
+            }
+            enqueueSnackbar(
+              t(
+                "nui_menu.player_modal.actions.moderation.kick_dialog.kick_sent"
+              ),
+              { variant: translateAlertType(resp.type) }
+            );
+          })
+          .catch((e) => {
+            enqueueSnackbar(
+              t(
+                "nui_menu.player_modal.actions.moderation.kick_dialog.unknown_error"
+              ),
+              { variant: "error" }
+            );
+            console.error(e);
+          });
       },
     });
   };
@@ -155,27 +220,42 @@ const DialogActionView: React.FC = () => {
   const handleHeal = () => {
     if (!userHasPerm("players.heal", playerPerms)) return showNoPerms("Heal");
 
-    fetchNui('healPlayer', { id: assocPlayer.id })
-    enqueueSnackbar(t("nui_menu.player_modal.actions.moderation.action_notifications.heal_player"), {variant: 'success'})
-  }
+    fetchNui("healPlayer", { id: assocPlayer.id });
+    enqueueSnackbar(
+      t(
+        "nui_menu.player_modal.actions.moderation.action_notifications.heal_player"
+      ),
+      { variant: "success" }
+    );
+  };
 
   const handleGoTo = () => {
     if (!userHasPerm("players.teleport", playerPerms))
       return showNoPerms("Teleport");
 
     closeMenu();
-    fetchNui('tpToPlayer', { id: assocPlayer.id })
-    enqueueSnackbar(t("nui_menu.player_modal.actions.moderation.action_notifications.tp_player"), {variant: 'success'})
-  }
+    fetchNui("tpToPlayer", { id: assocPlayer.id });
+    enqueueSnackbar(
+      t(
+        "nui_menu.player_modal.actions.moderation.action_notifications.tp_player"
+      ),
+      { variant: "success" }
+    );
+  };
 
   const handleBring = () => {
     if (!userHasPerm("players.teleport", playerPerms))
       return showNoPerms("Teleport");
 
     closeMenu();
-    fetchNui('summonPlayer', { id: assocPlayer.id })
-    enqueueSnackbar(t("nui_menu.player_modal.actions.moderation.action_notifications.bring_player"), {variant: 'success'})
-  }
+    fetchNui("summonPlayer", { id: assocPlayer.id });
+    enqueueSnackbar(
+      t(
+        "nui_menu.player_modal.actions.moderation.action_notifications.bring_player"
+      ),
+      { variant: "success" }
+    );
+  };
 
   const handleSpectate = () => {
     if (!userHasPerm("players.spectate", playerPerms))
@@ -186,58 +266,113 @@ const DialogActionView: React.FC = () => {
   };
 
   const handleWeed = () => {
-    if (!userHasPerm("players.troll", playerPerms))
-      return showNoPerms("Troll");
-    fetchNui('weedEffectPlayer', { id: assocPlayer.id })
-  }
+    if (!userHasPerm("players.troll", playerPerms)) return showNoPerms("Troll");
+    fetchNui("weedEffectPlayer", { id: assocPlayer.id });
+  };
 
   const handleDrunk = () => {
-    if (!userHasPerm("players.troll", playerPerms))
-      return showNoPerms("Troll");
-    fetchNui('drunkEffectPlayer', { id: assocPlayer.id })
-  }
+    if (!userHasPerm("players.troll", playerPerms)) return showNoPerms("Troll");
+    fetchNui("drunkEffectPlayer", { id: assocPlayer.id });
+  };
 
   const handleSetOnFire = () => {
-    if (!userHasPerm("players.troll", playerPerms))
-      return showNoPerms("Troll");
+    if (!userHasPerm("players.troll", playerPerms)) return showNoPerms("Troll");
 
-    fetchNui('setOnFire', { id: assocPlayer.id })
-  }
+    fetchNui("setOnFire", { id: assocPlayer.id });
+  };
 
   const handleWildAttack = () => {
-    if (!userHasPerm("players.troll", playerPerms))
-      return showNoPerms("Troll");
+    if (!userHasPerm("players.troll", playerPerms)) return showNoPerms("Troll");
 
-    fetchNui('wildAttack', { id: assocPlayer.id })
-  }
+    fetchNui("wildAttack", { id: assocPlayer.id });
+  };
+
+  const TooltipOverride: React.FC<TooltipProps> = (props) => (
+    <Tooltip
+      classes={{
+        tooltip: classes.tooltipOverride,
+      }}
+      {...props}
+    >
+      {props.children}
+    </Tooltip>
+  );
 
   return (
     <DialogContent>
       <Box pb={1}>
-        <Typography variant="h6">{t("nui_menu.player_modal.actions.title")}</Typography>
+        <Typography variant="h6">
+          {t("nui_menu.player_modal.actions.title")}
+        </Typography>
       </Box>
-      <Typography style={{ paddingBottom: 5 }}>{t("nui_menu.player_modal.actions.sections.moderation")}</Typography>
+      <Typography style={{ paddingBottom: 5 }}>
+        {t("nui_menu.player_modal.actions.sections.moderation")}
+      </Typography>
       <Box className={classes.actionGrid}>
-        <Button variant="outlined" color="primary" onClick={handleDM}>{t("nui_menu.player_modal.actions.moderation.options.dm")}</Button>
-        <Button variant="outlined" color="primary" onClick={handleWarn}>{t("nui_menu.player_modal.actions.moderation.options.warn")}</Button>
-        <Button variant="outlined" color="primary" onClick={handleKick}>{t("nui_menu.player_modal.actions.moderation.options.kick")}</Button>
-        <Button variant="outlined" color="primary" onClick={handleSetAdmin}>{t("nui_menu.player_modal.actions.moderation.options.set_admin")}</Button>
+        <Button variant="outlined" color="primary" onClick={handleDM}>
+          {t("nui_menu.player_modal.actions.moderation.options.dm")}
+        </Button>
+        <Button variant="outlined" color="primary" onClick={handleWarn}>
+          {t("nui_menu.player_modal.actions.moderation.options.warn")}
+        </Button>
+        <Button variant="outlined" color="primary" onClick={handleKick}>
+          {t("nui_menu.player_modal.actions.moderation.options.kick")}
+        </Button>
+        <Button variant="outlined" color="primary" onClick={handleSetAdmin}>
+          {t("nui_menu.player_modal.actions.moderation.options.set_admin")}
+        </Button>
       </Box>
       <Typography style={{ paddingBottom: 5 }}>
         {t("nui_menu.player_modal.actions.sections.interaction")}
       </Typography>
       <Box className={classes.actionGrid}>
-        <Button variant="outlined" color="primary" onClick={handleHeal}>{t("nui_menu.player_modal.actions.moderation.options.heal")}</Button>
-        <Button variant="outlined" color="primary" onClick={handleGoTo}>{t("nui_menu.player_modal.actions.moderation.options.go_to")}</Button>
-        <Button variant="outlined" color="primary" onClick={handleBring}>{t("nui_menu.player_modal.actions.moderation.options.bring")}</Button>
-        <Button variant="outlined" color="primary" onClick={handleSpectate}>{t("nui_menu.player_modal.actions.moderation.options.spectate")}</Button>
+        <Button variant="outlined" color="primary" onClick={handleHeal}>
+          {t("nui_menu.player_modal.actions.moderation.options.heal")}
+        </Button>
+        <Button variant="outlined" color="primary" onClick={handleGoTo}>
+          {t("nui_menu.player_modal.actions.moderation.options.go_to")}
+        </Button>
+        <Button variant="outlined" color="primary" onClick={handleBring}>
+          {t("nui_menu.player_modal.actions.moderation.options.bring")}
+        </Button>
+        <Button variant="outlined" color="primary" onClick={handleSpectate}>
+          {t("nui_menu.player_modal.actions.moderation.options.spectate")}
+        </Button>
       </Box>
-      <Typography style={{ paddingBottom: 5 }}>{t("nui_menu.player_modal.actions.sections.troll")}</Typography>
+      <Typography style={{ paddingBottom: 5 }}>
+        {t("nui_menu.player_modal.actions.sections.troll")}
+      </Typography>
       <Box className={classes.actionGrid}>
-        <Button variant="outlined" color="primary" onClick={handleWeed}>{t("nui_menu.player_modal.actions.troll.options.weed")}</Button>
-        <Button variant="outlined" color="primary" onClick={handleSetOnFire}>{t("nui_menu.player_modal.actions.troll.options.fire")}</Button>
-        <Button variant="outlined" color="primary" onClick={handleDrunk}>{t("nui_menu.player_modal.actions.troll.options.drunk")}</Button>
-        <Button variant="outlined" color="primary" onClick={handleWildAttack}>{t("nui_menu.player_modal.actions.troll.options.wild_attack")}</Button>
+        <TooltipOverride
+          title={t("nui_menu.player_modal.actions.troll.options.weed_desc")}
+        >
+          <Button variant="outlined" color="primary" onClick={handleWeed}>
+            {t("nui_menu.player_modal.actions.troll.options.weed")}
+          </Button>
+        </TooltipOverride>
+        <TooltipOverride
+          title={t("nui_menu.player_modal.actions.troll.options.fire_desc")}
+        >
+          <Button variant="outlined" color="primary" onClick={handleSetOnFire}>
+            {t("nui_menu.player_modal.actions.troll.options.fire")}
+          </Button>
+        </TooltipOverride>
+        <TooltipOverride
+          title={t("nui_menu.player_modal.actions.troll.options.drunk_desc")}
+        >
+          <Button variant="outlined" color="primary" onClick={handleDrunk}>
+            {t("nui_menu.player_modal.actions.troll.options.drunk")}
+          </Button>
+        </TooltipOverride>
+        <TooltipOverride
+          title={t(
+            "nui_menu.player_modal.actions.troll.options.wild_attack_desc"
+          )}
+        >
+          <Button variant="outlined" color="primary" onClick={handleWildAttack}>
+            {t("nui_menu.player_modal.actions.troll.options.wild_attack")}
+          </Button>
+        </TooltipOverride>
       </Box>
     </DialogContent>
   );
