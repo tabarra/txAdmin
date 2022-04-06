bdec85c09db416fb9501d20e1696b3232693f22a
Andre Tabarra <tabarra@vexguard.net>
2022-04-06 16:12:47 -0300

    chore: update released

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@
 author 'Tabarra'
 description 'Remotely Manage & Monitor your GTA5 FiveM Server'
 repository 'https://github.com/tabarra/txAdmin'
-version '4.13.3'
+version '4.13.4'
 ui_label 'txAdmin'
 
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
