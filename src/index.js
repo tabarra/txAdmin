9d2f08b5949972210eb31836d08c845973aa9c65
Andre Tabarra <tabarra@vexguard.net>
2022-05-11 16:25:58 -0300

    chore: version bump

--- src/index.js
+++ src/index.js
@@ -204,8 +204,8 @@ if (verbose) dir({isZapHosting, forceInterface, forceFXServerPort, txAdminPort, 
 //NOTE: Only valid if its being very actively maintained.
 //          Use 30d for patch 0, or 45~60d otherwise
 //      Objective is to update every 2~3 weeks, always on monday ~15:00
-const txVerBBLastUpdate = 1650299999;
-const txVerBBDelta = 23 + ((isZapHosting) ? 10 : 0);
+const txVerBBLastUpdate = 1652292073;
+const txVerBBDelta = 21 + ((isZapHosting) ? 10 : 0);
 const txAdminVersionBestBy = txVerBBLastUpdate + (txVerBBDelta * 86400);
 // dir({
 //     updateDelta: txVerBBDelta,
