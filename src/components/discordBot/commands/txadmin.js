fd1be4bfe92350aaf541e0ab8ed8d7d7f819e19e
Dolbajoba <83533764+Getpwnedbydolbajoba@users.noreply.github.com>
2022-07-01 22:05:16 +0300

    Maybe works but nice update :D
    If something doesn't work then DM me (Lil Dolbajoba#4276), ty.

--- src/components/discordBot/commands/txadmin.js
+++ src/components/discordBot/commands/txadmin.js
@@ -2,13 +2,14 @@
 const modulename = 'DiscordBot:cmd:txadmin';
 const { MessageEmbed } = require('@citizenfx/discord.js');
 const { dir, log, logOk, logWarn, logError } = require('../../../extras/console')(modulename);
-
+const packageJSON = require("./package.json");
+const discordJSVersion = packageJSON.dependencies["discord.js"];
 module.exports = {
     description: 'Prints the current txAdmin version',
     async execute(message, args) {
         const outMsg = new MessageEmbed({
             color: 0x4DEEEA,
-            title: `${globals.config.serverName} uses txAdmin v${GlobalData.txAdminVersion} :smiley:`,
+            title: `${globals.config.serverName} uses txAdmin v${GlobalData.txAdminVersion} & I'm running ${discordJSVersion} :smiley:`,
             description: 'Checkout the project:\n GitHub: https://github.com/tabarra/txAdmin\n Discord: https://discord.gg/f3TsfvD',
         });
         return await message.reply({embeds: [outMsg]});
