c144378857caa17929bdfa0e987128ba7acc752e
Andre Tabarra <tabarra@vexguard.net>
2022-08-08 22:38:11 -0300

    fix(menu/styles): fixed main page tooltip + updated mui

--- menu/src/components/misc/HelpTooltip.tsx
+++ menu/src/components/misc/HelpTooltip.tsx
@@ -1,41 +1,23 @@
 import React, { ReactNode } from "react";
-import {Fade, styled, Theme, Tooltip, Typography} from "@mui/material";
+import { Fade, styled, Typography } from "@mui/material";
+import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip';
 
 import { useTooltip } from "../../provider/TooltipProvider";
 
-const PREFIX = 'HelpTooltip'
-
-const classes = {
-  tooltip: `${PREFIX}-tooltip`,
-  arrow: `${PREFIX}-arrow`,
-}
-
-const StyledHelpTooltip = styled(Tooltip)(({theme}) => ({
-  [`& ${classes.tooltip}`]: {
+const StyledTooltip = styled(({ className, ...props }: TooltipProps) => (
+  <Tooltip {...props} arrow classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
     backgroundColor: theme.palette.background.default,
     color: theme.palette.text.primary,
     borderRadius: 10,
     padding: 10,
     transformOrigin: "bottom",
-    animationIterationCount: "infinite",
-    animationDuration: "2s",
-    animationTimingFunction: "ease",
   },
-  [`& ${classes.arrow}`]: {
+  [`& .${tooltipClasses.arrow}`]: {
     color: theme.palette.background.default,
   },
-  "@keyframes bounce-tool-tip": {
-    "0%": {
-      transform: "translateY(0)",
-    },
-    "50%": {
-      transform: "translateY(3px)",
-    },
-    "100%": {
-      transform: "translateY(0)",
-    },
-  },
-}))
+}));
 
 interface HelpTooltipProps {
   children: ReactNode;
@@ -45,17 +27,17 @@ export const HelpTooltip: React.FC<HelpTooltipProps> = ({ children }) => {
   const { tooltipText, tooltipOpen } = useTooltip();
 
   return (
-    <StyledHelpTooltip
+    <StyledTooltip
       open={tooltipOpen}
       title={
         <Typography variant="caption" align="center">
           {tooltipText}
         </Typography>
       }
-      PopperProps={{
-        container: () => document.getElementById("#root"),
-      }}
-      arrow
+      //FIXME: is it needed? it was there in Taso's version
+      // PopperProps={{
+      //   container: () => document.getElementById("#root"),
+      // }}
       TransitionComponent={Fade}
       TransitionProps={{
         timeout: {
@@ -64,12 +46,8 @@ export const HelpTooltip: React.FC<HelpTooltipProps> = ({ children }) => {
           exit: 500,
         },
       }}
-      classes={{
-        tooltip: classes.tooltip,
-        arrow: classes.arrow,
-      }}
     >
       <div>{children}</div>
-    </StyledHelpTooltip>
+    </StyledTooltip>
   );
 };
