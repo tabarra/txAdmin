5332ff73b19ab3e43c985b410969937afe819816
tabarra <tabarra@vexguard.net>
2022-10-28 08:04:37 -0300

    wip: wl reqs search with fuse.js

--- core/webroutes/whitelist/list.ts
+++ core/webroutes/whitelist/list.ts
@@ -1,8 +1,10 @@
 const modulename = 'WebServer:WhitelistList';
+import Fuse from "fuse.js";
 import PlayerDatabase from '@core/components/PlayerDatabase';
 import { DatabaseWhitelistApprovalsType, DatabaseWhitelistRequestsType } from '@core/components/PlayerDatabase/databaseTypes';
-import logger from '@core/extras/console.js';
+import logger, { ogConsole } from '@core/extras/console.js';
 import { Context } from 'koa';
+import cleanPlayerName from "@shared/cleanPlayerName";
 const { dir, log, logOk, logWarn, logError } = logger(modulename);
 
 
@@ -14,11 +16,11 @@ export default async function WhitelistList(ctx: Context) {
     const table = ctx.params.table;
 
     //Delegate to the specific handler
-    if(table === 'requests'){
+    if (table === 'requests') {
         return await handleRequests(ctx, playerDatabase);
-    }else if(table === 'approvals'){
+    } else if (table === 'approvals') {
         return await handleApprovals(ctx, playerDatabase);
-    }else{
+    } else {
         return ctx.send({ error: 'unknown table' });
     }
 };
@@ -27,21 +29,38 @@ export default async function WhitelistList(ctx: Context) {
 /**
  * Handles the search functionality.
  */
- async function handleRequests(ctx: Context, playerDatabase: PlayerDatabase) {
-    const sendTypedResp = (data: DatabaseWhitelistRequestsType[]) => ctx.send(data);
+async function handleRequests(ctx: Context, playerDatabase: PlayerDatabase) {
+    type resp = {
+        total: number;
+        newest: number; //for the ignore all button not remove any that hasn't been seeing by the admin
+        requests: DatabaseWhitelistRequestsType[];
+    }
+    const sendTypedResp = (data: resp) => ctx.send(data);
 
     const requests = playerDatabase.getWhitelistRequests().reverse();
-    return sendTypedResp(requests);
 
+    let filtered;
+    const searchName = ctx.request.query?.searchName;
+    if (typeof searchName === 'string') {
+        const fuse = new Fuse(requests, {
+            keys: ['playerPureName', 'discordTag'],
+            threshold: 0.3
+        });
+        const { pureName } = cleanPlayerName(searchName);
+        filtered = fuse.search(pureName).map(x => x.item);
+        // ogConsole.dir(filtered)
+    }
+
+    const toDisplay = filtered ?? requests;
+    return sendTypedResp({
+        total: requests.length,
+        newest: (requests.length) ? requests[0].tsLastAttempt : 0,
+        requests: toDisplay,
+    });
+
+    //fazer só com newer e older?
     //TODO: implement search and pagination
-    // if (ctx.request.query?.search) {
-    //     return await handleSearch(ctx, dbo);
-    // } else {
-    //     const respData = {
-    //         headerTitle: 'Whitelist',
-    //     };
-    //     return ctx.utils.render('main/whitelist', respData);
-    // }
+
 }
 
 
