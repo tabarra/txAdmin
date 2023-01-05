f000fe92ff9c20cd888d802ff533bb57d9cbe2c4
tabarra <tabarra@vexguard.net>
2023-01-04 23:19:21 -0300

    chore: updated some packages

--- .deploy.config.js
+++ .deploy.config.js
@@ -1,5 +1,5 @@
 export default {
-    fxserverPath: 'E:\\FiveM\\BUILDS\\6116\\fxserver.exe',
+    fxserverPath: 'E:\\FiveM\\BUILDS\\6185\\fxserver.exe',
     debouncerInterval: 250,
     preReleaseExpirationDays: 21,
     txAdminArgs: [
