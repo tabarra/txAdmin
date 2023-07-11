f75bd9683fd20abd68ad767e28ab4e603684a432
tabarra <tabarra@vexguard.net>
2023-07-11 14:15:20 -0300

    fix(core): fixed shutdownNoticeDelay being null

--- core/components/ConfigVault.js
+++ core/components/ConfigVault.js
@@ -248,7 +248,7 @@ export default class ConfigVault {
             cfg.fxRunner.logPath = cfg.fxRunner.logPath || `${this.serverProfilePath}/logs/fxserver.log`; //not in template
             cfg.fxRunner.autostart = (cfg.fxRunner.autostart === 'true' || cfg.fxRunner.autostart === true);
             cfg.fxRunner.restartDelay = parseInt(cfg.fxRunner.restartDelay) || 1250; //not in template
-            cfg.fxRunner.shutdownNoticeDelay = parseInt(cfg.fxRunner.shutdownNoticeDelay) ?? 5; //not in template
+            cfg.fxRunner.shutdownNoticeDelay = parseInt(cfg.fxRunner.shutdownNoticeDelay) || 5; //not in template
             cfg.fxRunner.quiet = (cfg.fxRunner.quiet === 'true' || cfg.fxRunner.quiet === true);
         } catch (error) {
             console.verbose.dir(error);
