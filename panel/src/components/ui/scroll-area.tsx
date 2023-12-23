1fd7330f60a940e942f3034c67376dff99942d76
tabarra <tabarra@vexguard.net>
2023-12-23 10:32:58 -0300

    wip: updated/added some shadcn components

--- panel/src/components/ui/scroll-area.tsx
+++ panel/src/components/ui/scroll-area.tsx
@@ -38,9 +38,7 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb
-      className="relative flex-1 rounded-full bg-border"
-    />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ))
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
