b0faea8a7864558bb051ad57ac7d4e3aa5eb4c14
tabarra <tabarra@vexguard.net>
2024-01-03 19:06:05 -0300

    fix: admin manager recursive iframe

--- panel/src/pages/Iframe.tsx
+++ panel/src/pages/Iframe.tsx
@@ -3,6 +3,8 @@ type Props = {
 };
 
 export default function Iframe({ legacyUrl }: Props) {
+    //NOTE: if you open adminManager with autofill, the autofill will continue in the searchParams
+    //This is an annoying issue to fix, so #wontfix
     const searchParams = location.search ?? '';
     const hashParams = location.hash ?? '';
     return (
