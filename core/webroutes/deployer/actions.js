9bea908836886b0c945a06234478ac150a457f1b
Blumlaut <13604413+Blumlaut@users.noreply.github.com>
2024-03-22 15:24:01 +0100

    fix(deployer): incorrect add_principal example line

--- core/webroutes/deployer/actions.js
+++ core/webroutes/deployer/actions.js
@@ -165,7 +165,7 @@ async function handleSetVariables(ctx) {
     });
     userVars.addPrincipalsMaster = (addPrincipalLines.length)
         ? addPrincipalLines.join('\n')
-        : '# Deployer Note: this admin master has no identifiers to be automatically added.\n# add_principal identifier.discord.111111111111111111 group.admin #example';
+        : '# Deployer Note: this admin master has no identifiers to be automatically added.\n# add_principal identifier.discord:111111111111111111 group.admin #example';
 
     //Start deployer
     try {
