87503993cd27b6e8db365f124b4fe6c02da4c7cf
futuristic-man <52795676+furcoite-nirual@users.noreply.github.com>
2025-02-28 15:35:46 +0100

    removed rdr3_warning

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,7 +8,6 @@ repository 'https://github.com/tabarra/txAdmin'
 version 'REPLACE-VERSION'
 ui_label 'txAdmin'
 
-rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 fx_version 'cerulean'
 game 'common'
 -- nui_callback_strict_mode 'true' --FIXME: menu iframe doesn't work
